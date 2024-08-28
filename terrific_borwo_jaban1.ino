f2154e0b7a60e07d875edad04ce4697b95eb68c2
strixt17 <nathanvarghese09@gmail.com>
2024-08-28 07:29:16 -0400

    2nd hour commt

---
+++ terrific_borwo_jaban1.ino
@@ -0,0 +1,77 @@
+// C++ code
+//
+
+int redPin1 = 2;
+int redPin2 = 9;
+int redPin3 = 10;
+int greenPin1 = 4;
+int greenPin2 = 7;
+int greenPin3 = 12;
+int bluePin1 = 5;
+int bluePin2 = 6;
+int bluePin3 = 11;
+int yellowPin1 = 3;
+int yellowPin2 = 8;
+
+
+void setup()
+{
+    pinMode(redPin1, OUTPUT);
+    pinMode(redPin2, OUTPUT);
+  pinMode(redPin3, OUTPUT);
+  pinMode(greenPin1, OUTPUT);
+  pinMode(greenPin2, OUTPUT);
+  pinMode(greenPin3, OUTPUT);
+  pinMode(bluePin1, OUTPUT);
+  pinMode(bluePin2, OUTPUT);
+  pinMode(bluePin3, OUTPUT);
+  pinMode(yellowPin1, OUTPUT);
+  pinMode(yellowPin2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(redPin1, HIGH);
+  digitalWrite(redPin2, HIGH);
+  delay(1000);
+  
+  digitalWrite(redPin3, 252);
+  digitalWrite(greenPin3, 0);
+  digitalWrite(bluePin3, 0);
+  
+  digitalWrite(redPin1, LOW);
+  digitalWrite(redPin2, LOW);
+               
+  digitalWrite(yellowPin1, HIGH);
+  digitalWrite(yellowPin2, HIGH);
+  delay(1000);
+  
+  digitalWrite(redPin3, 252);
+  digitalWrite(greenPin3, 252);
+  digitalWrite(bluePin3, 0);
+               
+  digitalWrite(yellowPin1, LOW);
+  digitalWrite(yellowPin2, LOW);
+  
+  digitalWrite(greenPin1, HIGH);
+  digitalWrite(greenPin2, HIGH);
+  delay(1000);
+               
+  digitalWrite(redPin3, 0);
+  digitalWrite(greenPin3, 252);
+  digitalWrite(bluePin3, 0);
+  
+  digitalWrite(greenPin1, LOW);
+  digitalWrite(greenPin2, LOW);
+  
+  digitalWrite(bluePin1, HIGH);
+  digitalWrite(bluePin2, HIGH);
+  delay(1000);
+               
+  digitalWrite(redPin3, 0);
+  digitalWrite(greenPin3, 0);
+  digitalWrite(bluePin3, 252);
+  
+  digitalWrite(bluePin1, LOW);
+  digitalWrite(bluePin2, LOW);
+}
