a266d0f4e9fd93caec6a6b2d6333fab1ab2ce7f5
strixt17 <nathanvarghese09@gmail.com>
2024-08-28 08:47:09 -0400

    3rd hour commit

--- terrific_borwo_jaban1.ino
+++ terrific_borwo_jaban1.ino
@@ -16,61 +16,61 @@ int yellowPin2 = 8;
 
 void setup()
 {
-    pinMode(redPin1, OUTPUT);
-    pinMode(redPin2, OUTPUT);
-  pinMode(redPin3, OUTPUT);
-  pinMode(greenPin1, OUTPUT);
-  pinMode(greenPin2, OUTPUT);
-  pinMode(greenPin3, OUTPUT);
-  pinMode(bluePin1, OUTPUT);
-  pinMode(bluePin2, OUTPUT);
-  pinMode(bluePin3, OUTPUT);
-  pinMode(yellowPin1, OUTPUT);
-  pinMode(yellowPin2, OUTPUT);
+  pinMode(redPin1, OUTPUT);
+  pinMode(redPin2, OUTPUT);
+	pinMode(redPin3, OUTPUT);
+	pinMode(greenPin1, OUTPUT);
+	pinMode(greenPin2, OUTPUT);
+	pinMode(greenPin3, OUTPUT);
+	pinMode(bluePin1, OUTPUT);
+	pinMode(bluePin2, OUTPUT);
+	pinMode(bluePin3, OUTPUT);
+	pinMode(yellowPin1, OUTPUT);
+	pinMode(yellowPin2, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(redPin1, HIGH);
   digitalWrite(redPin2, HIGH);
-  delay(1000);
   
   digitalWrite(redPin3, 252);
   digitalWrite(greenPin3, 0);
-  digitalWrite(bluePin3, 0);
+  digitalWrite(bluePin3, 0); 
+  delay(1000);
   
   digitalWrite(redPin1, LOW);
   digitalWrite(redPin2, LOW);
                
   digitalWrite(yellowPin1, HIGH);
   digitalWrite(yellowPin2, HIGH);
-  delay(1000);
   
   digitalWrite(redPin3, 252);
   digitalWrite(greenPin3, 252);
   digitalWrite(bluePin3, 0);
+  delay(1000);
                
   digitalWrite(yellowPin1, LOW);
   digitalWrite(yellowPin2, LOW);
   
   digitalWrite(greenPin1, HIGH);
   digitalWrite(greenPin2, HIGH);
-  delay(1000);
                
   digitalWrite(redPin3, 0);
   digitalWrite(greenPin3, 252);
   digitalWrite(bluePin3, 0);
+  delay(1000);
   
   digitalWrite(greenPin1, LOW);
   digitalWrite(greenPin2, LOW);
   
   digitalWrite(bluePin1, HIGH);
   digitalWrite(bluePin2, HIGH);
-  delay(1000);
                
   digitalWrite(redPin3, 0);
   digitalWrite(greenPin3, 0);
   digitalWrite(bluePin3, 252);
+  delay(1000);
   
   digitalWrite(bluePin1, LOW);
   digitalWrite(bluePin2, LOW);
